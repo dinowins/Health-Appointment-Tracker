bad85f88c5f798a7161ff4bfc3e77e519b062baf
Dino Nguyen <dino@>
2019-05-20 16:36:31 -0700

    Show dashboard component on app

---
+++ src/components/appointments/AppointmentList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const AppointmentList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <span className='card-title'>Project Title</span>
+        <p>Posted by: Dino Nguyen</p>
+        <p className='grey-text'>3rd September, 2am</p>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <span className='card-title'>Project Title</span>
+        <p>Posted by: Dino Nguyen</p>
+        <p className='grey-text'>3rd September, 2am</p>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <span className='card-title'>Project Title</span>
+        <p>Posted by: Dino Nguyen</p>
+        <p className='grey-text'>3rd September, 2am</p>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <span className='card-title'>Project Title</span>
+        <p>Posted by: Dino Nguyen</p>
+        <p className='grey-text'>3rd September, 2am</p>
+      </div>
+    </div>
+  )
+}
+
+export default AppointmentList;
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react'
 import Notifications from './Notifications'
+import AppointmentList from '../appointments/AppointmentList'
 
 class Dashboard extends Component {
   render(){
     return (
       <div className="dashboard containter">
         <div className='row'>
-          <div className='col s12 m6'></div>
+          <div className='col s12 m6'>
+            <AppointmentList/>
+          </div>
           <div className='col s12 m5 offset-m1'>
             <Notifications/>
           </div>
-        </>
+        </div>
       </div>
     )
   }
 }
+
+export default Dashboard;
