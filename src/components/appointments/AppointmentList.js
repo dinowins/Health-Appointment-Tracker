68219796030dfc4bd6a5bb91c83d49d73675b51a
Dino Nguyen <dinodnguyen@gmail.com>
2019-05-21 11:56:39 -0700

    Add summary to list component

--- src/components/appointments/AppointmentList.js
+++ src/components/appointments/AppointmentList.js
@@ -1,31 +1,13 @@
 import React from 'react'
+import AppointmentSummary from './AppointmentSummary'
 
 const AppointmentList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <span className='card-title'>Project Title</span>
-        <p>Posted by: Dino Nguyen</p>
-        <p className='grey-text'>3rd September, 2am</p>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <span className='card-title'>Project Title</span>
-        <p>Posted by: Dino Nguyen</p>
-        <p className='grey-text'>3rd September, 2am</p>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <span className='card-title'>Project Title</span>
-        <p>Posted by: Dino Nguyen</p>
-        <p className='grey-text'>3rd September, 2am</p>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <span className='card-title'>Project Title</span>
-        <p>Posted by: Dino Nguyen</p>
-        <p className='grey-text'>3rd September, 2am</p>
-      </div>
+      <AppointmentSummary />
+      <AppointmentSummary />
+      <AppointmentSummary />
+      <AppointmentSummary />
     </div>
   )
 }
---
+++ src/components/appointments/AppointmentSummary.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AppointmentSummary = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <span className='card-title'>Project Title</span>
+      <p>Posted by: Dino Nguyen</p>
+      <p className='grey-text'>3rd September, 2am</p>
+    </div>
+  )
+}
+
+export default AppointmentSummary
