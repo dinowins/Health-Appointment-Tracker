5181ff51b10f85a617e23318a9bfbb9c51837f01
Dino Nguyen <dinodnguyen@gmail.com>
2019-05-21 20:25:46 -0700

    Add links for signup and signin

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const SignedInLinks = () => {
   return (
     <ul className='right'>
-      <li><Link to='/'>New Appointment</Link></li>
+      <li><Link to='/create'>New Appointment</Link></li>
       <li><Link to='/'>Log Out</Link></li>
       <li><Link to='/' className='btn btn-floating pink lighten-1'>DN</Link></li>
     </ul>
--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const SignedOutLinks = () => {
   return (
     <ul className='right'>
-      <li><Link to='/'>Signup</Link></li>
-      <li><Link to='/'>Login</Link></li>
+      <li><Link to='/signup'>Signup</Link></li>
+      <li><Link to='/signin'>Login</Link></li>
     </ul>
   )
 }
