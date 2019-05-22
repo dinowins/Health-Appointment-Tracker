5a925b2371ba903a1424c2efb35dc2fe88390f92
Student <example@example.com>
2019-05-22 09:06:33 -0700

    Add firebase information

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'import firebase/auth'
+
+// Initialize Firebase
+
+var config = {
+  apiKey: "AIzaSyAKyHPxEXpzO5DtANfqZkGqxxB-XYrGCKw",
+  authDomain: "health-appointment-tracker.firebaseapp.com",
+  databaseURL: "https://health-appointment-tracker.firebaseio.com",
+  projectId: "health-appointment-tracker",
+  storageBucket: "health-appointment-tracker.appspot.com",
+  messagingSenderId: "528801567567"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase; 
