8cc0837b25e913b698c34dfe0a5ae4ac9d89ee61
Student <example@example.com>
2019-05-23 05:13:32 -0700

    format date with moment

--- src/components/appointments/AppointmentSummary.js
+++ src/components/appointments/AppointmentSummary.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import moment from 'moment'
 
 const AppointmentSummary = ({appointment}) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
         <span className='card-title'>{appointment.title}</span>
-        <p className='grey-text'>{appointment.date}</p>
+        <p className='grey-text'>{moment(appointment.date).format('MMM Do YYYY')}</p>
         <p className='grey-text'>{appointment.time}</p>
         <p className='grey-text'>{appointment.address}</p>
       </div>
