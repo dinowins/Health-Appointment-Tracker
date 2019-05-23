6f42d39a59cce18e2eb841b89e2c3f74e063882b
Student <example@example.com>
2019-05-23 05:39:47 -0700

    Add web font loader

--- src/index.js
+++ src/index.js
@@ -10,6 +10,13 @@ import thunk from 'redux-thunk';
 import { reduxFirestore, getFirestore } from 'redux-firestore';
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 import fbConfig from './config/fbConfig'
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Bree Serif', 'Fugaz One']
+  }
+})
 
 const store = createStore(rootReducer, 
   compose(
